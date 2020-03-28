1d5d4cff1a6aae6072f42fccf8116a47dd739fe0
AnuragDShetty <anuragshetty.1998@gmail.com>
2020-03-28 19:04:51 +0530

    First

--- app/src/main/java/com/example/plantscanner/MainActivity.java
+++ app/src/main/java/com/example/plantscanner/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.plantscanner;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawerlayout);
+        mToggle=new ActionBarDrawerToggle(MainActivity.this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView=(NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id= menuItem.getItemId();
+        if(id == R.id.login)
+        {
+            Toast.makeText(this,"This is login",Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.signup)
+        {
+            Toast.makeText(this,"This is Registration",Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.history)
+        {
+            Toast.makeText(this,"This is History",Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
